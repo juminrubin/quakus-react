72e1760e655985bf8babe07253dae262cc6e596c
JR <juminrubin@gmail.com>
2022-09-30 09:25:53 +0200

    adding back webapp folder

---
+++ src/main/webapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UploadFile from './components/UploadFiles';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Multiple File Upload</h2>
+      <UploadFile />
+    </div>
+  );
+}
+
+export default App;
